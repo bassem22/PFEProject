98c0522210689249538097aec02293d34095cd9a
bassem22 <bassembelmabrouk@gmail.com>
2019-04-20 16:38:44 +0200

    Update PresentationProjectApplicationTests.java

--- src/test/java/org/esprit/PresentationProjectApplicationTests.java
+++ src/test/java/org/esprit/PresentationProjectApplicationTests.java
@@ -9,6 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class PresentationProjectApplicationTests {
 
+	
+	
 	@Test
 	public void contextLoads() {
 		System.out.println("hello");
