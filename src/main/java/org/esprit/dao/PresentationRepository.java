99dc5b46cf1e9e4bd16881ca3d922f53a8cd21fc
bassem22 <bassembelmabrouk@gmail.com>
2019-04-20 16:44:47 +0200

    Update PresentationRepository.java

--- src/main/java/org/esprit/dao/PresentationRepository.java
+++ src/main/java/org/esprit/dao/PresentationRepository.java
@@ -4,5 +4,4 @@ import org.esprit.entities.Presentation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PresentationRepository extends JpaRepository<Presentation, Integer> {
-
 }
